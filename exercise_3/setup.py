bc9702865763be58a92cebd7137ddf37bdb3e729
xuanminh12995 <minh.vu@u.nus.edu>
2017-12-08 01:06:50 +0800

    Edit setup

--- exercise_3/setup.py
+++ exercise_3/setup.py
@@ -3,13 +3,10 @@
 from distutils.core import setup
 
 setup(
-    name='FullStackTemplate',
+    name='TableDashboard',
     version='1.0.0',
-    description='A Template for creating a Full Stack Web Application using Python, NPM, Webpack and React',
-    author='Angela Branaes',
-    author_email='angela.branaes10@alumni.imperial.ac.uk',
-    license='MIT',
-    url='https://github.com/angineering/FullStackTemplate',
+    description='A Table Dashboard using Python, NPM, Webpack and React',
+    author='Minh',
     keywords = [ 'fullstack', 'template', 'python', 'react','npm', 'webpack'],
     classifiers = [
     'Topic :: Software Development',
